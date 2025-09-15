47c25222065cf2e1d5ea1eea8df23694cf24fbbb
DiegoC <90105443+DiegoCivi@users.noreply.github.com>
2025-09-15 18:07:25 -0300

    Bump to cairo 2.12.3 (#79)
    * Bump to cairo 2.12.3
    
    * Update native ref and use tilde in toml
    
    * Avoid using cairo <2.12.3
    
    * Update native ref to main

--- crates/blockifier/src/execution/syscalls/hint_processor.rs
+++ crates/blockifier/src/execution/syscalls/hint_processor.rs
@@ -1,5 +1,6 @@
 use std::any::Any;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 use cairo_lang_casm::hints::Hint;
 use cairo_lang_runner::casm_run::execute_core_hint_base;
@@ -778,7 +779,6 @@ impl HintProcessorLogic for SyscallHintProcessor<'_> {
         vm: &mut VirtualMachine,
         exec_scopes: &mut ExecutionScopes,
         hint_data: &Box<dyn Any>,
-        _constants: &HashMap<String, Felt>,
     ) -> HintExecutionResult {
         let hint = hint_data.downcast_ref::<Hint>().ok_or(HintError::WrongHintData)?;
         // Segment arena finalization is relevant only for proof so there is no need to allocate
@@ -802,6 +802,7 @@ impl HintProcessorLogic for SyscallHintProcessor<'_> {
         _ap_tracking_data: &ApTracking,
         _reference_ids: &HashMap<String, usize>,
         _references: &[HintReference],
+        _constants: Rc<HashMap<std::string::String, cairo_vm::Felt252>>,
     ) -> Result<Box<dyn Any>, VirtualMachineError> {
         Ok(Box::new(self.hints[hint_code].clone()))
     }
